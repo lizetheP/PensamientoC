6f72a5f722374d1bab2f15b60b88d4894fdf5394
lizethefuertes <45633704+lizethefuertes@users.noreply.github.com>
2019-09-06 11:44:55 -0500

    vi

--- calendario/documentos/script.py
+++ calendario/documentos/script.py
@@ -6,20 +6,34 @@
 
 #
  
-base = 1
+"""base = 1
 altura = 2
 area = (base*altura)/2
-print("área triangulo", area)
+print("área triangulo", area)"""
 
-lado = 2
+def areaTriangulo(base, altura):
+    return (base*altura)/2
+
+res = areaTriangulo(1, 2)
+print("El área del triángulo es: ", res)
+
+"""lado = 2
 area = lado*lado
-print("área cuadrado", area)
+print("área cuadrado", area)"""
 
-base = 1
+def areaCuadrado(lado):
+    return lado*lado
+
+res = areaCuadrado(2)
+print("El área del cuadrado es: ", res)
+
+"""base = 1
 altura = 2
-area_triangulo = (base*altura)/2
+area_triangulo = (base*altura)/2"""
 
-lado = 2
-area_cuadrado = lado * lado
+area_triangulo = areaTriangulo(1,2)
+area_cuadrado = areaCuadrado(2)
+"""lado = 2
+area_cuadrado = lado * lado"""
 area_piramide = area_triangulo + area_cuadrado
 print("área pirámide", area_piramide)
