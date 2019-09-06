19dae7dfcc558c8cf09b612e246d7f409d7c8d84
lizethefuertes <45633704+lizethefuertes@users.noreply.github.com>
2019-09-06 15:33:39 -0500

    viernes

---
+++ calendario/documentos/areas.py
@@ -0,0 +1,11 @@
+base = 1
+altura = 2
+area_triangulo = (base*altura)/2
+print("área triangulo", area_triangulo)
+
+lado = 2
+area_cuadrado = lado*lado
+print("área cuadrado", area_cuadrado)
+
+area_piramide = area_triangulo + area_cuadrado
+print("área pirámide", area_piramide)
