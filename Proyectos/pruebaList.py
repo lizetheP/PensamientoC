e1c4c145ea94c9d6a5ed802a55b0c44d30a8614a
Lizethe <lizetheperez@gmail.com>
2021-09-23 21:19:45 -0500

    subir

---
+++ Proyectos/pruebaList.py
@@ -0,0 +1,11 @@
+palabra = "Python"
+lista = list(palabra)
+print(lista)
+
+string = "x, y, z"
+lista = string.split(',')
+print(lista)
+
+lista = [10, 12, 14, 16, 18, 20, 22, 24]
+del lista[3]
+print(lista)
