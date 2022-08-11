7d93e65476d9b55e961a1751f519618e95f8f428
Lizethe <lizetheperez@gmail.com>
2022-08-11 10:29:28 -0500

    subir

---
+++ programas/funcionImprime2.py
@@ -0,0 +1,11 @@
+def imprime_datos(num_lab):
+    print("Analítica de datos")
+    print("Laboratorio", num_lab)
+
+def main():
+    num = int(input("Introduce el numero de laboratorio: "))
+    imprime_datos(num)
+
+main()
+              
+              
