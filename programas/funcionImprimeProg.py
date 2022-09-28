32538ce6da06080ffe6a34ed009f520f35e9160d
Lizethe <lizetheperez@gmail.com>
2022-09-28 16:48:52 -0500

    subir

---
+++ programas/funcionImprimeProg.py
@@ -0,0 +1,11 @@
+def imprime_datos(num_lab):
+    print("Programación para negocios")
+    print("Laboratorio", num_lab)
+
+def main():
+    num = int(input("Introduce el numero de laboratorio: "))
+    imprime_datos(num)
+
+main()
+              
+              
